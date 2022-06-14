70056ad876ad6e6201bc6717646dc6d1fd43e02f
Serhii-P <serhii.pyskun@gmail.com>
2022-06-14 11:40:13 +0300

    changed route

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Provider } from 'react-redux'
 import { store } from './redux/store/store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
   </React.StrictMode>
 );
